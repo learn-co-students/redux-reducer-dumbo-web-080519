9cc28dce91b6a1a8a48c8053d1b724615196f2ce
Zermina Ejaz <zerminaejaz1018@gmail.com>
2019-10-25 15:39:49 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
 export function manageFriends(state, action){
+    switch(action.type){
+
+        case("ADD_FRIEND"):
+            if(state.friends.length == 0){
+                return {friends: [action.friend]}
+            }
+            else
+                return {friends: [...state.friends,action.friend]}   
+        
+        case("REMOVE_FRIEND"):
+            let friend = state.friends.find(friend=>{
+                return friend.id === action.id
+            })
+            console.log("State Friend", friend)
+                let index = state.friends.indexOf(friend)
+                let newArray = state.friends
+                newArray.splice(index,1)
+                return {friends: newArray}
+
+        default:
+            return state
+    }
+
 }
